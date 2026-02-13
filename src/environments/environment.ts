b7f3cc466e485a152872864b8f3e5e1e1cfee687
sumanth <sumanthnagireddi@gmail.com>
2026-02-13 12:29:15 +0530

    ENV FILES UPDATED

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,4 @@
 export const environment = {
   production: true,
   API_URL: 'https://webservices-rqvr.onrender.com',
-  firebase:{
-      projectId: 'sumanthnagireddi-interview',
-      appId: '1:690782833147:web:85e61606f3f76fe1b7a35e',
-      storageBucket: 'sumanthnagireddi-interview.appspot.com',
-      apiKey: 'AIzaSyC_y_0iJAjkfcgsSIawGrcIOIPvF19sxi4',
-      authDomain: 'sumanthnagireddi-interview.firebaseapp.com',
-      messagingSenderId: '690782833147',
-      measurementId: 'G-S8V59ELZD2',
-    },
 };
